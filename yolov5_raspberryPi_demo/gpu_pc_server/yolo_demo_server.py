9e52934f33c77314c8ac74cc45e2ea84da9d91ab
Wei <58371015+weigao-123@users.noreply.github.com>
2022-05-06 18:14:27 -0600

    Update yolo_demo_server.py
    provide multiple clients concurrently

--- yolov5_raspberryPi_demo/gpu_pc_server/yolo_demo_server.py
+++ yolov5_raspberryPi_demo/gpu_pc_server/yolo_demo_server.py
@@ -4,6 +4,7 @@ import pickle
 import struct
 import time
 import torch
+from _thread import start_new_thread
 
 
 # Some Yolo parameters
@@ -12,23 +13,10 @@ NMS_THRESHOLD = 0.4
 COLORS = [(0, 255, 255), (255, 255, 0), (0, 255, 0), (255, 0, 0)]
 
 
-def start_yolo_server(model):
-    # Port for comunication
-    HOST = ''
-    PORT = 8000
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    print('Socket created')
-    s.bind((HOST, PORT))
-    print('Socket bind complete')
-    s.listen(10)
-    print('Socket now listening')
-
-    conn, addr = s.accept()
-
+def multi_threaded_client(conn, addr, model, show_video=True):
     data = b""
     payload_size = struct.calcsize(">L")
-    print("payload_size: {}".format(payload_size))
+    # print("payload_size: {}".format(payload_size))
     while True:
         while len(data) < payload_size:
             # print("Recv: {}".format(len(dataset)))
@@ -53,20 +41,42 @@ def start_yolo_server(model):
         result = model(frame)
         end = time.time()
 
-        start_drawing = time.time()
-        # Render the result in the video stream
-        result.render()
-        end_drawing = time.time()
+        if show_video:
+            start_drawing = time.time()
+            # Render the result in the video stream
+            result.render()
+            end_drawing = time.time()
 
-        # Calculate the FPS
-        fps_label = "FPS: %.2f" % (1 / (end - start))
-        cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
-        cv2.imshow("detections", frame)
+            # Calculate the FPS
+            fps_label = "FPS: %.2f" % (1 / (end - start))
+            cv2.putText(frame, fps_label, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 0), 2)
+            cv2.imshow("detections for {0}".format(addr), frame)
+            cv2.waitKey(1)
 
         # Inference results back to raspberry Pi
         back_data = result.pandas().xyxy[0].to_json()
         conn.sendall(bytes(back_data, encoding='utf-8'))
-        cv2.waitKey(1)
+    conn.close()
+
+def start_yolo_server(model):
+    # Port for comunication
+    HOST = ''
+    PORT = 8000
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print('Socket created')
+    s.bind((HOST, PORT))
+    print('Socket bind complete')
+    s.listen(10)
+    print('Socket now listening')
+
+    ThreadCount = 0
+    while True:
+        conn, addr = s.accept()
+        start_new_thread(multi_threaded_client, (conn, addr, model,))
+        ThreadCount += 1
+        print('Thread Count: ', ThreadCount)
+    s.close()
 
 
 if __name__ == "__main__":
@@ -77,5 +87,3 @@ if __name__ == "__main__":
     #model = torch.hub.load('ultralytics/yolov5', 'custom', path=r'custom_trained_model.pt')
 
     start_yolo_server(model)
-
-
